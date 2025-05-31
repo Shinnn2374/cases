bedca79afe4f84218cf854b911172ec8f105ea79
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-05-31 20:57:11 +0300

    ...

---
+++ NineCase/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Synchronizer synchronizer = new Synchronizer();
+        // Создаем несколько потоков, которые будут ждать
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                try {
+                    System.out.println(Thread.currentThread().getName() + " is waiting");
+                    synchronizer.blockUntilDone();
+                    System.out.println(Thread.currentThread().getName() + " is released");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+
+        // Ждем 2 секунды перед вызовом doOnce()
+        Thread.sleep(2000);
+        System.out.println("Calling doOnce()");
+        synchronizer.doOnce();
+    }
+}
---
+++ NineCase/src/main/java/org/example/Synchronizer.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Synchronizer {
+    private boolean isDone = false;
+    private final Object lock = new Object();
+
+    public void blockUntilDone() throws InterruptedException {
+        synchronized (lock) {
+            while (!isDone) {
+                lock.wait();
+            }
+        }
+    }
+
+    public void doOnce() {
+        synchronized (lock) {
+            isDone = true;
+            lock.notifyAll();
+        }
+    }
+}
